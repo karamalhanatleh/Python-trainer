04137d09180e9d1896581f32ac84f33d293b5cee
Karam ALhanatleh <93527002+karamalhanatleh@users.noreply.github.com>
2022-11-25 13:57:52 +0300

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 14 21:53:26 2022
+
+@author: Karam Alhanatlh
+"""
+from config import *
+from assembly_ai_plus.assemblyai import AssemblyAI
+import assembly_ai_plus as aa
